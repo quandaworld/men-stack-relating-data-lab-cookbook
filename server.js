520bce2457088fbdb86cf6383ff4ebc3daab5044
quandaworld <nhiquannguyen@gmail.com>
2025-03-16 23:07:10 -0700

    Use food controller and middleware in server

--- server.js
+++ server.js
@@ -7,9 +7,13 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
-const authController = require('./controllers/auth.js');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
-const port = process.env.PORT ? process.env.PORT : '3000';
+const authController = require('./controllers/auth.js');
+const foodsController = require('./controllers/food.js');
+
+const port = process.env.PORT ? process.env.PORT : '3001';
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -42,7 +46,10 @@ app.get('/vip-lounge', (req, res) => {
   }
 });
 
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/foods', foodsController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
