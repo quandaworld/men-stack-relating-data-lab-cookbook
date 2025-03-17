20fe1fd18c7d8a5c5adb8b096ec729bafd8c8718
quandaworld <nhiquannguyen@gmail.com>
2025-03-17 08:32:42 -0700

    Fix userId bugs

--- controllers/food.js
+++ controllers/food.js
@@ -5,10 +5,8 @@ const User = require('../models/user');
 // GET /users/:userId/foods
 router.get('/', async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId);
-    res.locals.pantry = user.pantry;
-    res.locals.userId = req.params.userId;
-    res.render('foods/index.ejs');
+    const user = await User.findById(req.session.user._id);
+    res.render('foods/index.ejs', { pantry: user.pantry });
   } catch (err) {
     console.log(err);
     res.redirect('/');
@@ -17,28 +15,39 @@ router.get('/', async (req, res) => {
 
 // GET /users/:userId/foods/new
 router.get('/new', (req, res) => {
-  res.render('foods/new.ejs', { userId: req.params.userId });
+  res.render('foods/new.ejs');
 });
 
 // POST /users/:userId/foods
 router.post('/', async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId);
+    const user = await User.findById(req.session.user._id);
     user.pantry.push(req.body);
     await user.save();
-    res.redirect(`/users/${req.params.userId}/foods`);
+    res.redirect(`/users/${user._id}/foods`);
   } catch (err) {
     console.log(err);
     res.redirect('/');
   }
 });
 
+router.get('/:foodId', async (req, res) => {
+  try {
+      const user = await User.findById(req.session.user._id);
+      const food = user.pantry.id(req.params.itemId);
+      res.render('foods/show.ejs', { food });
+  } catch (error) {
+      console.log(error);
+      res.redirect('/');
+  }
+});
+
 // GET /users/:userId/foods/:itemId/edit
 router.get('/:itemId/edit', async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId);
+    const user = await User.findById(req.session.user._id);
     const food = user.pantry.id(req.params.itemId);
-    res.render('foods/edit.ejs', { userId: req.params.userId, food });
+    res.render('foods/edit.ejs', { food });
   } catch (err) {
     console.log(err);
     res.redirect('/');
@@ -48,11 +57,11 @@ router.get('/:itemId/edit', async (req, res) => {
 // PUT /users/:userId/foods/:itemId
 router.put('/:itemId', async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId);
+    const user = await User.findById(req.session.user._id);
     const food = user.pantry.id(req.params.itemId);
     food.set(req.body);
     await user.save();
-    res.redirect(`/users/${req.params.userId}/foods`);
+    res.redirect(`/users/${user._id}/foods/`);
   } catch (err) {
     console.log(err);
     res.redirect('/');
@@ -62,10 +71,10 @@ router.put('/:itemId', async (req, res) => {
 // DELETE /users/:userId/foods/:itemId
 router.delete('/:itemId', async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId);
-    user.pantry.id(req.params.itemId).remove();
+    const user = await User.findById(req.session.user._id);
+    user.pantry.id(req.params.itemId).deleteOne();
     await user.save();
-    res.redirect(`/users/${req.params.userId}/foods`);
+    res.redirect(`/users/${user._id}/foods/${req.params.foodId}`);
   } catch (err) {
     console.log(err);
     res.redirect('/');
