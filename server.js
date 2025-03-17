622430dc87134c0d91d5cf46a2427196c07f76b3
quandaworld <nhiquannguyen@gmail.com>
2025-03-17 00:45:21 -0700

    Connect pass uver to view middleware

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/food.js');
-const usersController = require('./controllers/users.js'); // Import users controller
+const usersController = require('./controllers/user.js'); // Import users controller
 
 const port = process.env.PORT ? process.env.PORT : '3001';
 
@@ -29,6 +31,8 @@ app.use(
     saveUninitialized: true,
   })
 );
+
+app.use(passUserToView);
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {
