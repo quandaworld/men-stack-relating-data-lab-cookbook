55ca233fc65f350dba3824ab3cb53e89e89d63f2
quandaworld <nhiquannguyen@gmail.com>
2025-03-16 23:45:21 -0700

    Connect user controller in server.js

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
-const isSignedIn = require('./middleware/is-signed-in.js');
-const passUserToView = require('./middleware/pass-user-to-view.js');
-
 const authController = require('./controllers/auth.js');
 const foodsController = require('./controllers/food.js');
+const usersController = require('./controllers/users.js'); // Import users controller
 
 const port = process.env.PORT ? process.env.PORT : '3001';
 
@@ -38,18 +36,9 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
-});
-
-app.use(passUserToView);
 app.use('/auth', authController);
-app.use(isSignedIn);
 app.use('/users/:userId/foods', foodsController);
+app.use('/users', usersController); // Use users controller
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
